e558c15d9943b652b195412f5ffaf4950127d73f
jonathan Salguero <jsalg024@fiu.edu>
2021-07-22 11:22:47 -0400

    herokiu

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.item_routes import item_routes
-from .api.chat_routes import chat_routes
+#from .api.chat_routes import chat_routes
 
 from .seeds import seed_commands
 
